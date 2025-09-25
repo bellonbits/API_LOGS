6b3cdd63ee58091b1c0908867e3a05984c216f7f
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2025-09-25 16:28:34 +0300

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import pandas as pd
+import numpy as np
+
+app = FastAPI()
+
+# Load dataset when the server starts
+df = pd.read_csv("App_Logs.csv")
+df = df.replace([np.nan, np.inf, -np.inf], None)
+data_json = df.to_dict(orient="records")
+
+@app.get("/data")
+async def get_data():
+    return {"data": data_json}
